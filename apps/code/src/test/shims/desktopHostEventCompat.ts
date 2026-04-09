f4de00ef15b595830d494d0aa1e092066a8812a7
HugeX <mrleap002@gmail.com>
2026-04-09 03:45:32 +0000

    fix: remove tauri event shim import

--- apps/code/src/test/shims/desktopHostEventCompat.ts
+++ apps/code/src/test/shims/desktopHostEventCompat.ts
@@ -1,10 +1,1 @@
-import { listen as tauriListen } from "@tauri-apps/api/event";
-
-export type UnlistenFn = () => void;
-
-export function listen<T>(
-  eventName: string,
-  listener: (event: { payload: T }) => void
-): Promise<UnlistenFn> {
-  return tauriListen<T>(eventName, listener);
-}
+export { listen, type UnlistenFn } from "../../application/runtime/ports/desktopHostEvent";
